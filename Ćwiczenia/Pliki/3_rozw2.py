b12a1c78fc5a79717940f75d6bd4d755a63bd90b
Jeremi Maciejewski <jermac@st.amu.edu.pl>
2026-03-29 15:34:23 +0200

    Port the remaining parts of the course to HTML pages: - Ported lessons 3 and 4 to HTML; - Corrected links in index.html; - Small corrections in style.css; - Added HTML versions of exercises; - Added .py solutions to these exercises.

---
+++ Ćwiczenia/Pliki/3_rozw2.py
@@ -0,0 +1,6 @@
+sentence = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+
+# Dokończ tę linijkę
+number_of_i = sentence.count('i')
+
+print("Number of letters 'i' in first sentence of Lorem ipsum:", number_of_i)
